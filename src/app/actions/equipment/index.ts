9d860c9a6314f5356a42840b361873f86895e543
Paweł Rosa <76450441+Pablo203@users.noreply.github.com>
2024-06-01 23:00:32 +0200

    Revert "[feat]: Add quantity"

--- src/app/actions/equipment/index.ts
+++ src/app/actions/equipment/index.ts
@@ -4,4 +4,3 @@ export * from './a_equipment_create';
 export * from './a_equipment_delete';
 export * from './a_equipment_reservation_create';
 export * from './a_equipment_reservation_delete';
-export * from './a_quantity_add';
--- src/app/shared/services/actionService/action.service.ts
+++ src/app/shared/services/actionService/action.service.ts
@@ -3,36 +3,42 @@ import { WizardService } from '../wizardService/wizard.service';
 import { YesNoService } from '../yesNoService/yes-no.service';
 import { ApiService } from '../apiService/api.service';
 import { ViewService } from '../viewService/view.service';
-import { ToastService } from '../toastService/toast.service';
+
 import {
   a_equipment_type_create,
   a_equipment_type_delete,
   a_equipment_create,
   a_equipment_delete,
-  a_equipment_add_quantity
+  a_equipment_reservation_create,
+  a_equipment_reservation_delete
 } from 'src/app/actions/equipment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActionService {
+
   constructor(
     private wizardService: WizardService,
     private yesNoService: YesNoService,
     private apiService: ApiService,
     private viewService: ViewService,
-    private toastService: ToastService,
   ) { }
+
   executeFunction(functionName: string) {
     (this as any)[functionName]().subscribe();
   }
+
+  // EQUIPMENT
+  createEquipment() { return a_equipment_create(this.viewService, this.apiService, this.wizardService) }
+  deleteEquipment() { return a_equipment_delete(this.viewService, this.apiService, this.yesNoService) }
+
   // EQUIPMENT TYPE
   createEquipmentType() { return a_equipment_type_create(this.viewService, this.apiService, this.wizardService) }
   deleteEquipmentType() { return a_equipment_type_delete(this.viewService, this.apiService, this.yesNoService) }
 
-  
-  // EQUIPMENT
-  createEquipment() { return a_equipment_create(this.viewService, this.apiService, this.wizardService) }
-  deleteEquipment() { return a_equipment_delete(this.viewService, this.apiService, this.yesNoService) }
-  addQuantityEquipment() { return a_equipment_add_quantity(this.viewService, this.apiService, this.wizardService) }
+  // EQUIPMENT RESERVATION
+  createEquipmentReservation() { return a_equipment_reservation_create(this.viewService, this.apiService, this.wizardService) }
+  deleteEquipmentReservation() { return a_equipment_reservation_delete(this.viewService, this.apiService, this.yesNoService) }
+
 }
