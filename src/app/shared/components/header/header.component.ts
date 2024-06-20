34fd9c19bee44a32d92151c70b9e4147590ec0ab
Paweł Rosa <76450441+Pablo203@users.noreply.github.com>
2024-06-20 19:19:56 +0200

    Clean project structure (#46)
    Co-authored-by: a4ch3r <a4ch3r@a4ch3r.a4ch3r>

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { LoginService } from '../../services/loginService/login.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  constructor(private renderer: Renderer2, private loginService: LoginService) {}
+
+  ngOnInit(): void{
+    const body = document.body;
+    const className = 'toggle-sidebar';
+    this.renderer.addClass(body, className);
+  }
+
+  toggleSidebar(): void {
+    if (!this.loginService.getIsLogged()) return;
+    const body = document.body;
+    const className = 'toggle-sidebar';
+    
+    if (body.classList.contains(className)) {
+      this.renderer.removeClass(body, className);
+    } else {
+      this.renderer.addClass(body, className);
+    }
+  }
+}
